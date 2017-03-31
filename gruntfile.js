f43cf0a7fc5c28c4852b6e4784ca01ff61e23898
Carlyse Jordan <carlyse.jordan@Carlyses-Air.lan>
2017-03-31 15:01:19 -0400

    adding grunt and package.json

---
+++ gruntfile.js
@@ -0,0 +1,63 @@
+(function() {
+  'use strict';
+
+  module.exports = function(grunt) {
+    grunt.initConfig({
+      concat: {
+        alljs: {
+          options: {
+            sourceMap: true
+          },
+          src: ['/src/js/shop.module.js','src/js/**/*.s'],
+          dest: 'build/js/app.js'
+
+        }
+      },
+      babel: {
+
+        all: {
+          options: {
+            presets: ['es2015'],
+            sourceMap: true
+          },
+          dist: {
+            files: {
+              'build/js/app.js': 'build/js/app.js'
+            }
+          }
+        }
+
+      },
+
+      karma: {
+
+        all: {
+          options: {
+            frameworks: ['mocha','chai'],
+            browsers: ['Chrome'],
+            files: [
+              'node_modules/angular/angular.js',
+              'node_modules/angular-mocks/angular-mocks.js',
+              'src/js/shop.module.js',
+              'src/js/**/*.js',
+              'tests/**/*.js'
+            ],
+            singleRun: true,
+            preprocessors: {
+              'src/js/**/*.js': ['coverage']
+            },
+            reporters: ['dots', 'coverage'],
+            coverageReporter: {
+              type: 'text-summary'
+            }
+          }
+        }
+      }
+
+    });
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.registerTask('build', ['karma','concat','babel']);
+  };
+}());
