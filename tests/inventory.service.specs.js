12a63d74ff540d53beb7adfbe546ee9a7e3361f7
Carlyse Jordan <carlyse.jordan@Carlyses-MacBook-Air.local>
2017-04-01 14:36:07 -0400

    passed a length test

--- tests/inventory.service.specs.js
+++ tests/inventory.service.specs.js
@@ -14,9 +14,14 @@
     it('should be able to give an array of inventory', function(){
       let result = InventoryService.getAllInventory();
       expect(result).to.be.an('array');
+    });
+    it('item name should be a string', function(){
+      let item = {name: 'ufuhjfj', price: 789789};
+      InventoryService.addItemToInventory(item);
 
-
-
+      let result = InventoryService.getAllInventory();
+      expect(result.length).to.equal(1);
+      expect(result[0].name).to.be.a('string');
     });
 
 
